d93969d7ffed3c05171437196cf56f106508fe22
Michelle Watson <alicerockshard@gmail.com>
2023-02-25 23:18:47 -0500

    used time as x-axis, changed to area plot

--- EEfromRR.py
+++ EEfromRR.py
@@ -64,41 +64,63 @@ class PolarUser:
 
         return ee_list
 
+    # Convert RR-interval into time stamp (x-axis of graph)
+    # It's easier than parsing the time from the text file directly, but can only be used with RR-interval data
+    def rrtotime(self, rr):
+        time = []
+        running_t_ms = 0
+
+        # the running sum of rr-intervals is the same as the time value
+        for rr_interval in rr:
+            running_t_ms += rr_interval
+            running_t_min = round(running_t_ms / 60000.0, 2)
+            time.append(running_t_min)
+
+        return time
+
+
     # Convert text file to EE
     def texttoee(self, filename):
         # Extract rr from text file
         rr = self.loggertxt_to_rrlist_csv(filename)
 
+        time = self.rrtotime(rr)
+
         # convert RR to HR
         heart_rates = self.rrtohr(rr)
 
         # convert HR to EE
-        return self.hrtoee(heart_rates)
+        ee = self.hrtoee(heart_rates)
 
+        return time, ee
 
+# hack: instead of parsing time stamp, I could just use the RR itnerval as my time stamps
 # Create Polar user
 ethan = PolarUser("Ethan", 22, 1, 80)  # name, age, gender [male=1], weight [kg]
 
 # Process RR interval data into energy expenditure (EE) using Keytel's formula
-ethan_ee1 = ethan.texttoee('Ethan/ethan_BA_60_RR_1.txt')
-ethan_ee2 = ethan.texttoee('Ethan/ethan_BA_60_RR_2.txt')
-ethan_ee3 = ethan.texttoee('Ethan/ethan_BA_60_RR_3.txt')
-ethan_ee4 = ethan.texttoee('Ethan/ethan_BA_60_RR_4.txt')
-ethan_ee5 = ethan.texttoee('Ethan/ethan_BA_60_RR_5.txt')
+time_ethan_ee1, ethan_ee1 = ethan.texttoee('Ethan/ethan_BA_60_RR_1.txt')
+time_ethan_ee2, ethan_ee2 = ethan.texttoee('Ethan/ethan_BA_60_RR_2.txt')
+time_ethan_ee3, ethan_ee3 = ethan.texttoee('Ethan/ethan_BA_60_RR_3.txt')
+time_ethan_ee4, ethan_ee4 = ethan.texttoee('Ethan/ethan_BA_60_RR_4.txt')
+time_ethan_ee5, ethan_ee5 = ethan.texttoee('Ethan/ethan_BA_60_RR_5.txt')
 
 ##########################################################
+print(time_ethan_ee1)
 # Visualize data
 
 # create the plot
-plt.plot(ethan_ee1, label='EE Baseline 1')
-plt.plot(ethan_ee2, label='EE Baseline 2')
-plt.plot(ethan_ee3, label='EE Baseline 3')
-plt.plot(ethan_ee4, label='EE Baseline 4')
-plt.plot(ethan_ee5, label='EE Baseline 5')
+plt.fill_between(time_ethan_ee1, ethan_ee1, color='red', alpha=0.5, label='EE Baseline 1')
+plt.fill_between(time_ethan_ee2, ethan_ee2, color='orange', alpha=0.5, label='EE Baseline 2')
+plt.fill_between(time_ethan_ee3, ethan_ee3, color='yellow', alpha=0.5, label='EE Baseline 3')
+plt.fill_between(time_ethan_ee4, ethan_ee4, color='#90EE90', alpha=0.5, label='EE Baseline 4')
+plt.fill_between(time_ethan_ee5, ethan_ee5, color='#87CEFA', alpha=0.5, label='EE Baseline 5')
+
+
 
 # set the axis labels and title
-plt.xlabel('Sample Number')
-plt.ylabel('Energy Expenditure')
+plt.xlabel('Time [minutes]')
+plt.ylabel('Energy Expenditure [kJ/minute]')
 plt.title('Ethan\'s Baseline Energy Expenditure Carrying 60kg Uphill')
 
 # add a legend to the plot
