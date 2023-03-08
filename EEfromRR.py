b732720553b552f0e9661df63fcbc11c61aa53cd
Michelle Watson <alicerockshard@gmail.com>
2023-03-07 19:44:36 -0500

    added avg EE and peak HR calcs + plots

--- EEfromRR.py
+++ EEfromRR.py
@@ -2,6 +2,7 @@ import numpy as np  # linear algebra
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 import csv  # better library compared to panda for less complicated text files
 import matplotlib.pyplot as plt
+from statistics import mean
 
 import os
 
@@ -83,11 +84,15 @@ class PolarUser:
     # trim HR at peak
     def trim_hr(self, t, hr):
         maxVal = max(hr)
-        print(maxVal)
         i = hr.index(maxVal)
+        # All trials took ~30s, so if the maxHR is hit too late or too early, just trim to a standard t=0.7s
+        if t[i] >= 0.7 or t[i] < 0.1:
+            print(t[i])
+            i = next(x for x, val in enumerate(t) if val > 0.7)
+            print(i)
         trimHR = hr[0:i]
         trimt = t[0:i]
-        return trimt, trimHR
+        return trimt, trimHR, maxVal
 
     # trim HR ~30s
 
@@ -101,57 +106,89 @@ class PolarUser:
         # convert RR to HR
         heart_rates = self.rrtohr(rr)
 
+        if filename == 'Ethan/ethan_BA_60_RR_2.txt':
+            with open('HR.txt', 'w') as f:
+                write = csv.writer(f)
+                # using csv.writer method from CSV package
+                write.writerow(heart_rates)
+
         # trim HR when it peaks, need to trim time as well since they're plotted together
-        t_trim, hr_trim = self.trim_hr(time, heart_rates)
+        t_trim, hr_trim, peak_hr = self.trim_hr(time, heart_rates)
 
         # convert HR to EE
         ee = self.hrtoee(hr_trim)
+        print(ee)
+        avg_ee = mean(ee)
 
-        return t_trim, rr, hr_trim, ee
+        return t_trim, rr, hr_trim, ee, round(avg_ee), round(peak_hr)
 
-    def process_files(self, type, filename_base_str, time_base_str, HR_trim_base_str, ee_base_str):
+    # def process_files(self, type, filename_base_str, time_base_str, HR_trim_base_str, ee_base_str, eeavg_base_str, hrpeak_base_str):
+    def process_files(self, type, filename_base_str):
         base_string = 'Ethan/ethan_BA_60_RR_'
         colour_list = ['red', 'orange', 'yellow', '#90EE90', '#87CEFA']
 
+        # keep track of peak HR and avg EE through trials
+        peakHR_list = []
+        avgEE_list = []
+
         # create a 2x2 subplot grid
-        fig, axs = plt.subplots(nrows=2, ncols=1, figsize=(10, 10))
+        fig, axs = plt.subplots(nrows=4, ncols=1, figsize=(10, 10))
+        fig.tight_layout(h_pad=2)
 
         # flatten the axs array to make it easier to iterate over
         axs = axs.flatten()
 
-        # Assign names
-        ax1 = axs[0]
-        ax2 = axs[1]
+        # Keep track of subplots
+        ax1 = axs[0]    # HR v time
+        ax2 = axs[1]    # EE v time
+        ax3 = axs[2]
+        ax4 = axs[3]
 
         for i in range(1, 6):
             file_path = f"{filename_base_str}{i}.txt"
-            t_str = f"{time_base_str}{i}"
-            hr_str = f"{HR_trim_base_str}{i}"
-            ee_str = f"{ee_base_str}{i}"
+            # t_str = f"{time_base_str}{i}"
+            # hr_str = f"{HR_trim_base_str}{i}"
+            # ee_str = f"{ee_base_str}{i}"
+            # avg_ee_str = f"{eeavg_base_str}{i}"
+            # peak_hr_str = f"{hrpeak_base_str}{i}"
 
             if os.path.isfile(file_path):
                 # Perform some action on the file
                 print(f"Processing file: {file_path}")
 
                 # Process RR interval data into energy expenditure (EE) using Keytel's formula
-                t_str, _, hr_str, ee_str = self.texttoee(file_path)
+                t_str, _, hr_str, ee_str, avg_ee_str, peak_hr_str = self.texttoee(file_path)
+                peakHR_list.append(peak_hr_str)
+                avgEE_list.append(avg_ee_str)
 
                 # Plot HR
-                ax1.fill_between(t_str, hr_str, color=colour_list[i - 1], alpha=0.5, label=f'HR {type} {i}')
+                ax1.fill_between(t_str, hr_str, color=colour_list[i - 1], alpha=0.5, label=f'HR {type} {i} peak = {peak_hr_str} bpm')
 
                 # Plot EE
-                ax2.fill_between(t_str, ee_str, color=colour_list[i - 1], alpha=0.5, label=f'EE {type} {i}')
-
+                ax2.fill_between(t_str, ee_str, color=colour_list[i - 1], alpha=0.5, label=f'EE {type} {i}, avg = {avg_ee_str} kJ/min')
 
             else:
                 print(f"File not found: {file_path}")
 
+        # Plot peak HR over trials
+        ax3.plot(peakHR_list)
+        ax3.set_xlabel('Trial')
+        ax3.set_ylabel('Heart Rate [beats/minute]')
+        ax3.set_title(f'Peak HR for {type} {base_string}')
+
+        # Plot avg EE over trials
+        ax4.plot(avgEE_list)
+        ax4.set_xlabel('Trial')
+        ax4.set_ylabel('Energy Expenditure [kJ/minute]')
+        ax4.set_title(f'Average EE for {type} {base_string}')
+
         # set the axis labels and title
         ax1.set_xlabel('Time [minutes]')
-        ax2.set_xlabel('Time [minutes]')
         ax1.set_ylabel('Heart Rate [beats/minute]')
-        ax2.set_ylabel('Energy Expenditure [kJ/minute]')
         ax1.set_title(f'{type} HR for {base_string}')
+
+        ax2.set_xlabel('Time [minutes]')
+        ax2.set_ylabel('Energy Expenditure [kJ/minute]')
         ax2.set_title(f'{type} EE for {base_string}')
 
         # add a legend to the plot
@@ -165,12 +202,13 @@ class PolarUser:
 
 
 # hack: instead of parsing time stamp, I could just use the RR interval as my time stamps
-# Create Polar user
+# Create Polar user and Process files
 ethan = PolarUser("Ethan", 22, 1, 80)  # name, age, gender [male=1], weight [kg]
-# Process files
-ethan.process_files('baseline', 'Ethan/ethan_BA_60_RR_', 'time_ethan_ee_', 'ethan_hr_trim_', 'ethan_ee_')
+ethan.process_files('baseline', 'Ethan/ethan_BA_60_RR_')
+
 
 '''
+ethan.process_files('baseline', 'Ethan/ethan_BA_60_RR_', 'time_ethan_ee_', 'ethan_hr_trim_', 'ethan_ee_', 'ethan_avg_ee', 'ethan_peak_hr')
 # Process RR interval data into energy expenditure (EE) using Keytel's formula
 time_ethan_ee1, _, _, ethan_ee1 = ethan.texttoee('Ethan/ethan_BA_60_RR_1.txt')
 time_ethan_ee2, _, _, ethan_ee2 = ethan.texttoee('Ethan/ethan_BA_60_RR_2.txt')
